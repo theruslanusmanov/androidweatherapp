5499b9b51c1f08bb1e9e60252fcd56efc29cc616
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-11-28 07:41:11 +0500

    add hilt dep

---
+++ app/src/main/java/com/theruslanusmanov/androidweatherapp/AndroidWeatherApplication.kt
@@ -0,0 +1,11 @@
+package com.theruslanusmanov.androidweatherapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AndroidWeatherApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
