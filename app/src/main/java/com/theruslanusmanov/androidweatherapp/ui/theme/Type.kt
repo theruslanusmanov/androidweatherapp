837230f9ed887597bf5ca643e6d706e2bcd6fa99
Ruslan Usmanov <ruslanusm6@gmail.com>
2023-01-25 00:26:17 +0500

    optimize imports

--- app/src/main/java/com/theruslanusmanov/androidweatherapp/ui/theme/Type.kt
+++ app/src/main/java/com/theruslanusmanov/androidweatherapp/ui/theme/Type.kt
@@ -2,12 +2,10 @@ package com.theruslanusmanov.androidweatherapp.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.ExperimentalTextApi
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.googlefonts.Font
 import androidx.compose.ui.text.googlefonts.GoogleFont
-import androidx.compose.ui.unit.sp
 import com.theruslanusmanov.androidweatherapp.R
 
 @OptIn(ExperimentalTextApi::class)
