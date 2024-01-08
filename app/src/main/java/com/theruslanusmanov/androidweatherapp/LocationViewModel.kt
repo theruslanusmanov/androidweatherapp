b32e2906191cae99842fc512b7355cd25a2d5196
Ruslan Usmanov <ruslanusm6@gmail.com>
2024-01-08 18:08:14 +0300

    add example data

--- app/src/main/java/com/theruslanusmanov/androidweatherapp/LocationViewModel.kt
+++ app/src/main/java/com/theruslanusmanov/androidweatherapp/LocationViewModel.kt
@@ -1,21 +1,24 @@
 package com.theruslanusmanov.androidweatherapp
 
 import android.util.Log
+import androidx.datastore.preferences.core.doublePreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.core.stringSetPreferencesKey
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
-object UserScheme {
-    val FIELD_NAME = stringPreferencesKey("name")
+object LocationScheme {
+    val FIELD_CITY = stringPreferencesKey("city")
+    val FIELD_LATITUDE = doublePreferencesKey("latitude")
+    val FIELD_LONGITUDE = doublePreferencesKey("longitude")
 }
 
 @HiltViewModel
@@ -33,7 +36,9 @@ class LocationViewModel @Inject constructor(
 
     private fun getLocation() = viewModelScope.launch {
         locationRepository.dataStore.edit { prefs ->
-            prefs[UserScheme.FIELD_NAME] = "John"
+            prefs[LocationScheme.FIELD_CITY] = "Berlin"
+            prefs[LocationScheme.FIELD_LATITUDE] = 52.520008
+            prefs[LocationScheme.FIELD_LONGITUDE] = 13.404954
         }
         locationRepository.dataStore.data.collect() {
             _currentLocation.value = it.toString()
