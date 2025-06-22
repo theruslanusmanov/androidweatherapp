f9ce95bef51172434ecd7cef745422d7574921ce
Ruslan Usmanov <Ruslan.Usmanov@mvideo.ru>
2025-06-22 22:10:33 +0300

    refac

--- app/src/main/java/com/theruslanusmanov/androidweatherapp/weather/WeatherApp.kt
+++ app/src/main/java/com/theruslanusmanov/androidweatherapp/weather/WeatherApp.kt
@@ -2,9 +2,7 @@ package com.theruslanusmanov.androidweatherapp.weather
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -36,7 +34,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.theruslanusmanov.androidweatherapp.R
 import com.theruslanusmanov.androidweatherapp.WeatherRoutes
 import com.theruslanusmanov.androidweatherapp.ui.theme.weatherTypography
