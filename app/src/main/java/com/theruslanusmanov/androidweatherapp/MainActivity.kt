e13a735d449fb26fb63867f6d7e80161d4be257b
Ruslan Usmanov <ruslanusm6@gmail.com>
2026-03-21 18:46:48 +0300

    add search animation

--- app/src/main/java/com/theruslanusmanov/androidweatherapp/MainActivity.kt
+++ app/src/main/java/com/theruslanusmanov/androidweatherapp/MainActivity.kt
@@ -5,6 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.core.EaseOut
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -45,7 +52,28 @@ class MainActivity : ComponentActivity() {
                             WeatherView(onSearch = {navController.navigate(WeatherRoutes.Search)})
                         }
                         // * Search
-                        composable<WeatherRoutes.Search> {
+                        composable<WeatherRoutes.Search>(
+                            enterTransition = {
+                                fadeIn(
+                                    animationSpec = tween(
+                                        300, easing = LinearEasing
+                                    )
+                                ) + slideIntoContainer(
+                                    animationSpec = tween(300, easing = EaseIn),
+                                    towards = AnimatedContentTransitionScope.SlideDirection.Start
+                                )
+                            },
+                            exitTransition = {
+                                fadeOut(
+                                    animationSpec = tween(
+                                        300, easing = LinearEasing
+                                    )
+                                ) + slideOutOfContainer(
+                                    animationSpec = tween(300, easing = EaseOut),
+                                    towards = AnimatedContentTransitionScope.SlideDirection.End
+                                )
+                            }
+                        ) {
                             SearchView(onBack = {navController.navigate(WeatherRoutes.Main)})
                         }
                     }
